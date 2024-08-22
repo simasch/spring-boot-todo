71e4c7985dc8cd09c1f1117ddcd1e5c4255127fd
Simon Martinelli <simon@72.services>
2024-08-22 15:15:01 +0200

    Removed superfluous method

--- src/main/java/ch/martinelli/demoweb/repository/TodoRepository.java
+++ src/main/java/ch/martinelli/demoweb/repository/TodoRepository.java
@@ -2,12 +2,6 @@ package ch.martinelli.demoweb.repository;
 
 import ch.martinelli.demoweb.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
-
-    @Query("select max(t.id) from Todo t")
-    Optional<Integer> findMaxId();
 }
